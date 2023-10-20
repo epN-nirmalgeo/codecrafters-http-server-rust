30d0b12be69c2fca19deafe2d5ee64c5f76c3b0a
epN-nirmalgeo <nirmalgeo@gmail.com>
2023-10-19 21:00:09 -0700

    minor fix

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ fn main() {
     let listener = TcpListener::bind("localhost:4221").unwrap();
     const GET: &str = "GET";
     const ECHO: &str = "/echo/";
+    const RESPONSE_OK: &[u8; 19] = b"HTTP/1.1 200 OK\r\n\r\n";
     const RESPONSE_NOT_FOUND: &[u8; 26] = b"HTTP/1.1 404 NOT FOUND\r\n\r\n";
 
     for stream_result in listener.incoming() {
@@ -16,7 +17,9 @@ fn main() {
                 let lines: Vec<&str>= request.lines().collect();
                 let line_token: Vec<&str> = lines[0].split(' ').collect();
 
-                if line_token[0] == GET && line_token[1].starts_with(ECHO) {
+                if line_token[0] == GET && line_token[1] == "/" {
+                    let _ = stream.write_all(RESPONSE_OK);
+                } else if line_token[0] == GET && line_token[1].starts_with(ECHO) {
                     let word = &line_token[1][6..];
                     let len = word.len();
                     let s = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}\r\n", len, word);
