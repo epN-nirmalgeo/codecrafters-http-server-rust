28bc50ff2bcd7bc1a871048707a90ec6910b6a3d
epN-nirmalgeo <nirmalgeo@gmail.com>
2023-10-19 19:44:33 -0700

    404 handler

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
-use std::{net::TcpListener, io::Write};
+use std::{net::TcpListener, io::{Write, Read}};
 
 fn main() {
 
     let listener = TcpListener::bind("localhost:4221").unwrap();
+    const GET_REQUEST: &str= "GET / HTTP/1.1";
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut _stream) => {
-                let _ = _stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n");
+    for stream_result in listener.incoming() {
+        match stream_result {
+            Ok(mut stream) => {
+                let mut data = [0u8; 4096];
+                stream.read(&mut data).unwrap();
+                let request = String::from_utf8_lossy(&data);
+                for line in request.lines() {
+                    println!("{line}");
+                    if line == GET_REQUEST {
+                        let _ = stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n");
+                    } else {
+                        let _ = stream.write_all(b"HTTP/1.1 404 NOT FOUND\r\n\r\n");
+                    }
+                    break;
+                }
+
+
             } 
             Err(e) => {
                 println!("error : {}", e);
